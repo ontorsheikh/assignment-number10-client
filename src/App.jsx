7af0f8c786f0ebbd194437ee03c43fd2f343855a
ontorsheikh <skontorsheikh1613@gmail.com>
2025-11-13 15:33:32 +0600

    completed navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { Outlet } from "react-router";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-      
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,101 @@
+import { Link, NavLink } from "react-router";
+
+const Header = () => {
+  const user = true;
+  const menuLinks = (
+    <>
+        <NavLink to="/" className="btn">
+          Home
+        </NavLink>
+        <NavLink to="/foods" className="btn">
+          Foods
+        </NavLink>
+        <NavLink to="/about" className="btn">
+          About
+        </NavLink>
+        <NavLink to="/contact" className="btn">
+          Contact
+        </NavLink>
+    </>
+  );
+  const rightMenu = (
+    <>
+      {user ? (
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="m-1 cursor-pointer">
+            <img
+              src="https://assignment-no10-private.web.app/user.png"
+              alt=""
+              className="w-10 h-10 rounded-full cursor-pointer"
+            />
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-fit p-2 shadow-sm"
+          >
+            <li>
+              <span>name</span>
+            </li>
+            <li>
+              <span>email sdggfgdfg</span>
+            </li>
+            <li>
+              <span className="block">
+                <button className="btn btn-error text-white w-full">
+                  Log Out
+                </button>
+              </span>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className="flex gap-2 items-center">
+          <Link to="/login" className="btn btn-outline btn-secondary">
+            Login
+          </Link>
+          <Link to="/register" className="btn btn-secondary">
+            Register
+          </Link>
+        </div>
+      )}
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {menuLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Food Lovers</a>
+        <ul className="menu menu-horizontal px-1 hidden lg:flex gap-2">
+          {menuLinks}
+        </ul>
+      </div>
+      <div className="navbar-end">{rightMenu}</div>
+    </div>
+  );
+};
+
+export default Header;
